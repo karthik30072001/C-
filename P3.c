ce459718a495d0ceea32ac97d0683811b79435f9
karthik30072001 <75940485+karthik30072001@users.noreply.github.com>
2021-03-26 10:16:32 +0530

    Add files via upload
    To check a ASCII value  of a character!

---
+++ P3.c
@@ -0,0 +1,9 @@
+            /*This program for calculate the ASCII VALUE*/
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a character:\n");
+    scanf("%c", &c);
+    printf("ASCII Value of %c is %d\n", c ,c);
+}
