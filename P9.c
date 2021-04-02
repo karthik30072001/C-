8c94d03a9c4b2ff71428b8461bc69b3788061339
karthik30072001 <75940485+karthik30072001@users.noreply.github.com>
2021-04-02 13:27:05 +0530

    Add files via upload

---
+++ P9.c
@@ -0,0 +1,15 @@
+                            //while loop concept     
+#include<stdio.h>
+int main()
+{
+    int i=1,j=10;
+    int sum;
+   while(i<=5)    
+    {
+        
+        printf("%d\n", i*j);            
+        i++;
+        j--;
+    }
+
+}
