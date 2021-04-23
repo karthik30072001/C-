b38b9e4274e44d6f78ee3f221254cf49ce14dcf5
karthik30072001 <75940485+karthik30072001@users.noreply.github.com>
2021-04-23 13:03:28 +0530

    Add files via upload

---
+++ Square _root/square_root.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double num,root;
+    printf("Enter any number to find square root:\n");
+    scanf("%lf",&num);
+    root = sqrt(num);
+    printf("\nSquare root of the %.2lf is %.2lf\n", num, root);
+    return 0;
+}
